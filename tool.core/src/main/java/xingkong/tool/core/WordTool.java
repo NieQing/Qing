28c8a833348b8a283bda12ac625d3fc20a4baae9
星空 <27447985@qq.com>
2019-05-30 10:59:36 +0800

    添加Word工具

--- tool.core/src/main/java/xingkong/tool/core/WordTool.java
+++ tool.core/src/main/java/xingkong/tool/core/WordTool.java
@@ -25,61 +25,98 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class WordTool {
-	
+
 	public static void printTableH(String fileName) throws FileNotFoundException, IOException {
 		HWPFDocument document = new HWPFDocument(new FileInputStream(fileName));
 		Range range = document.getRange();
+		
+		 //获取段落数
+		   int paraNum = range.numParagraphs();
+		   System.out.println(paraNum);
+		   for (int i=0; i<10; i++) {
+//		    this.insertInfo(range.getParagraph(i));
+		     System.out.println("段落" + (i+1) + "：" + range.getParagraph(i).text());
+		     
+		     
+		   }
+		
+		
 		TableIterator talbleIter = new TableIterator(range);
 		// 获取所有表格
-		
-		while (talbleIter.hasNext()) {
-		    // 获取表格的行
-		    Table table = (Table)talbleIter.next();
-		    for (int i=0; i<table.numRows(); i++) {
-		        // 获取表格的每个单元格
-		        TableRow tr = table.getRow(i);
-		        for (int j=0; j<tr.numCells();j++) {
-		             // 获取单元格的内容
-		            TableCell cell = tr.getCell(j); 
-		            String text = "";
-		        	for(int k=0;k<cell.numParagraphs();k++)
-		        	{
-		        		Paragraph para = cell.getParagraph(k);
-		        		text+=para.text().trim()+" ";
-		        	}
-		            System.out.print(text + " ");
-		        }
-		        System.out.println();
-		    }
-		    break;
+
+		for (int m = 0; m < 2; m++) {
+			// 获取表格的行
+			if (talbleIter.hasNext()) {
+				Table table = (Table) talbleIter.next();
+				if(table.numRows()==1)
+				{
+					// 跳过空表格
+					break;
+				}
+				for (int i = 0; i < table.numRows(); i++) {
+					// 获取表格的每个单元格
+					TableRow tr = table.getRow(i);
+					for (int j = 0; j < tr.numCells(); j++) {
+						// 获取单元格的内容
+						TableCell cell = tr.getCell(j);
+						String text = "";
+						for (int k = 0; k < cell.numParagraphs(); k++) {
+							Paragraph para = cell.getParagraph(k);
+							text += para.text().trim() + " ";
+						}
+						System.out.print(text + " ");
+					}
+					System.out.println();
+				}
+			}
 		}
 	}
-	
+
 	public static void printTable(String fileName) throws FileNotFoundException, IOException {
 		XWPFDocument document = new XWPFDocument(new FileInputStream(fileName));
 		// 获取所有表格
 		List<XWPFTable> tables = document.getTables();
 		for (XWPFTable table : tables) {
-		    // 获取表格的行
-		    List<XWPFTableRow> rows = table.getRows();
-		    for (XWPFTableRow row : rows) {
-		        // 获取表格的每个单元格
-		        List<XWPFTableCell> tableCells = row.getTableCells();
-		        for (XWPFTableCell cell : tableCells) {
-		             // 获取单元格的内容
-		             String text = cell.getText();
-		             System.out.print(text + " ");
-		        }
-		        System.out.println();
-		    }
-		    break;
+			// 获取表格的行
+			List<XWPFTableRow> rows = table.getRows();
+			int i = 1;
+			for (XWPFTableRow row : rows) {
+				// 获取表格的每个单元格
+				List<XWPFTableCell> tableCells = row.getTableCells();
+
+				for (int j = 0; j < tableCells.size(); j++) {
+					XWPFTableCell cell = tableCells.get(j);
+					// 获取单元格的内容
+					String text = cell.getText();
+					if (j == (tableCells.size() - 1)) {
+						System.out.print(text);
+					} else {
+						System.out.print(text + " | ");
+					}
+
+				}
+				if (i == 1) {
+					System.out.println();
+					for (int k = 0; k < tableCells.size() - 1; k++) {
+
+						if (k == (tableCells.size() - 2)) {
+							System.out.print("-");
+						} else {
+							System.out.print("-|");
+						}
+					}
+					i++;
+				}
+				System.out.println();
+			}
+			System.out.println();
+			System.out.println();
 		}
 	}
-	
-	
+
 	public static void main(String[] args) {
 		try {
-			printTableH("E:\\test2.doc");
+			printTableH("E:\\test4.doc");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
